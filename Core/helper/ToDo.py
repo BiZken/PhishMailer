fd5abed645563c4a97c40405a030833e325de79e
BiZken <30388479+BiZken@users.noreply.github.com>
2020-05-16 23:39:45 +0200

    Added ToDo List

---
+++ Core/helper/ToDo.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from .color import green, white, blue, start, alert, numbering
+
+def TODO():
+	print(start + " ToDo List " + start)
+	print(alert + " This List Will Be Updated" + alert)
+	print("")
+	print(numbering(1) + white + " Add More Email Template " + numbering(1))
+	print(numbering(2) + white + " Create Better Interface (might take a while) " + numbering(2))
+	print(numbering(3) + white + " Add Mass Email Sender " + numbering(3))
+	print(numbering(4) + white + " Add Emails With Different Languages " + numbering(4))
+	print(numbering(5) + white + " Add More Target Specified Emails " + numbering(5))
+	
+
