bb155f7f935e26f7236e735be63ccd4a1f774e94
BiZken <30388479+BiZken@users.noreply.github.com>
2020-05-16 17:38:51 -0400

    Added More Email Services

--- Core/mailer.py
+++ Core/mailer.py
@@ -1,15 +1,9 @@
-import smtplib
 import os
-import getpass
 import sys
-import ssl
-from email.mime.text import MIMEText
-from email.utils import formataddr
-from email.mime.multipart import MIMEMultipart  
-from email.mime.base import MIMEBase  
-from email import encoders  
-from email.mime.text import MIMEText
 from Core.helper.color import green, white, blue, start, alert, numbering
+from Core.Mailer.gmail import Gmail
+from Core.Mailer.yahoo import YahooEmail
+from Core.Mailer.live import LiveEmail
 
 def NormalEmail():
 	os.system("clear")
@@ -17,35 +11,35 @@ def NormalEmail():
 	print("""
  __^__                                                        __^__
 ( ___ )------------------------------------------------------( ___ )
- | / |                                                        | \\ |
- | / |+-------------)PhishMailer BaitMailer V1(--------------+| \\ |
+ | / |                                                        | \ |
+ | / |+------------)PhishMailer BaitMailer V1.5(-------------+| \ |
  |___|                                                        |___|
 (_____)------------------------------------------------------(_____) """)
-
-	print(alert + "It Might Take A Few Minutes Until The Target Gets The Email" + alert)
-	print(alert + "You Need To Allow Less Secure Apps On You Gmail Account" + alert)
-	
 	print("")
-	fromaddr = input(start + " Enter Your Email-Address: ")
-	password = getpass.getpass(start + " Enter Your Password (will not be shown): ")
-	toaddr = input(start + " Enter Email-Address To Send To: ")
-	subject = input(start + " Enter Subject: ")
-	pathfile = input(start + " Enter Path To Html File: ")
-
-	html = open(pathfile)
-	msg = MIMEText(html.read(), 'html')
-	msg['From'] = fromaddr
-	msg['To'] = toaddr
-	msg['Subject'] = subject
-
-	debug = False
-	if debug:
-		print(msg.as_string())
+	print(numbering(1) + white + " @Gmail")
+	print(numbering(2) + white + " @Yahoo")
+	print(numbering(3) + white + " @Hotmail")
+	print(numbering(4) + white + " @Outlook")
+	print(numbering(5) + white + " @Live")
+	print(alert + " There Might Be Some Bugs, Please Open An Issue On Github If Found! " + alert)
+	
+	Smtp_pick = int(input("\nroot@phishmailer/Mailer/:~ "))
+	
+	if Smtp_pick == 1:
+		Gmail()
+		
+	elif Smtp_pick == 2:
+		YahooEmail()
+	
+	elif Smtp_pick == 3:
+		LiveEmail()
+		
+	elif Smtp_pick == 4:
+		LiveEmail()
+		
+	elif Smtp_pick == 5:
+		LiveEmail()
+		
 	else:
-		server = smtplib.SMTP('smtp.gmail.com',587)
-		server.starttls()
-		server.login(fromaddr, password)
-		text = msg.as_string()
-		server.sendmail(fromaddr, toaddr, text)
-		server.quit()
-		print(alert + "Email Sent" + alert)
+		print(alert + " Invalid Option " + alert) 
+	
