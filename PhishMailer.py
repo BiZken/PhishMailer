f21cd31e63a8a674fbd41230995b0eddc8d33a3a
D3adpool2K <32305505+Deadpool2000@users.noreply.github.com>
2021-11-17 19:59:15 +0530

    Fix : ValueError when input is str (#34)
    When we input selection as 'str' instead of 'int', exception occurs at line 65 -
    
    Traceback (most recent call last):
      File "/home/user/Tool/PhishMailer/PhishMailer.py", line 183, in <module>
        mainMenu()
      File "/home/user/Tool/PhishMailer/PhishMailer.py", line 65, in mainMenu
        mailPick = int(input(green + "root@phishmailer:~ " + white))
    ValueError: invalid literal for int() with base 10: 'er'

--- PhishMailer.py
+++ PhishMailer.py
@@ -64,120 +64,127 @@ def mainMenu():
 	
 	mailPick = int(input(green + "root@phishmailer:~ " + white))
 
-	if mailPick == 1:
-		Instagram()
+	try:
 
-	elif mailPick == 2:
-		Facebook()
-		
-	elif mailPick == 3:
-		Gmail()
+            if mailPick == 1:
+                    Instagram()
 
-	elif mailPick == 4:
-		GmailActivity()
+            elif mailPick == 2:
+                    Facebook()
 
-	elif mailPick == 5:
-		Twitter()
+            elif mailPick == 3:
+                    Gmail()
 
-	elif mailPick == 6:
-		Snapchat()
+            elif mailPick == 4:
+                    GmailActivity()
 
-	elif mailPick == 7:
-		SnapchatSimple()
+            elif mailPick == 5:
+                    Twitter()
 
-	elif mailPick == 8:
-		Steam()
-		
-	elif mailPick == 9:
-		Dropbox()
-		
-	elif mailPick == 10:
-		Linkedin()
-		
-	elif mailPick == 11:
-		Playstation()
-		
-	elif mailPick == 12:
-		Paypal1()
-		
-	elif mailPick == 13:
-		Discord()
-		
-	elif mailPick == 14:
-		Spotify()
-		
-	elif mailPick == 15:
-		Blockchain()
-		
-	elif mailPick == 16:
-		RiotGames()
-		
-	elif mailPick == 17:
-		Rockstar()
+            elif mailPick == 6:
+                    Snapchat()
 
-	elif mailPick == 18:
-		AskFM()
+            elif mailPick == 7:
+                    SnapchatSimple()
 
-	elif mailPick == 19:
-		Webhost000()
-		
-	elif mailPick == 20:
-		Dreamteam()
-		
-	elif mailPick == 21:
-		print("Gamehag Coming Soon")
-	
-	elif mailPick == 22:
-		Mega()
+            elif mailPick == 8:
+                    Steam()
 
-	elif mailPick == 30:
-		MailerMenu()
-	
-	elif mailPick == 69:
-		RedirectionMain()
+            elif mailPick == 9:
+                    Dropbox()
 
-	elif mailPick == 80:
-		Languages()
+            elif mailPick == 10:
+                    Linkedin()
 
-	elif mailPick == 99:
-		os.system("clear")
-		print("Hope I See You Soon")
-		print("Happy Phishing")
-		sys.exit()
+            elif mailPick == 11:
+                    Playstation()
 
-	elif mailPick == 1337:
-		print("\n" + green + "[" + white + "+" + green + "]" + white + " This Tool Was Created So People Would Have It Easier To Launch Phishing Attacks")
-		print("\n" + green + "[" + white + "+" + green + "]" + white + " I Do Not Take Any Responsibility For Your Actions")
-		print("\n" + green + "[" + white + "+" + green + "]" + white + " But I Don't Give A F*ck About What You Do")
-		print("\n" + green + "[" + white + "+" + green + "]" + white + " More Emails Will Come Soon!")
-		print("\n" + green + "[" + white + "+" + green + "]" + white + " Contact:")
-		print(green + "[" + white + "+" + green + "]" + white + " Instagram: BiZk3n (most Active)")
-		print(green + "[" + white + "+" + green + "]" + white + " Github: BiZken [https://github.com/BiZken]")
-		CurrentDir()
-		print(green + "[" + white + "+" + green + "]" + white + " Restart PhishMailer? Y/N")
-		
-		ReRun = input("root@phishmailer:~ " + white)
-		if ReRun == "Y" or ReRun == "y":
-			os.system("clear")
-			mainMenu()
-		else:
-			os.system("clear") 
-			print(alert + " shuting Down")
-		
-	elif mailPick == 4444:
-		TODO()
-		print(start + " Restart PhishMailer? Y/N")
-		restart = input("root@phishmailer:~ " + white)
-		if restart == "Y" or restart == "y":
-			os.system("clear")
-			mainMenu()
-		else: 
-			print(alert + " shuting Down")
-			sys.exit()
+            elif mailPick == 12:
+                    Paypal1()
 
-	else:
-		print("\nSomething Went Wrong There Partner")
-		print("Are You Ok? Did You Fell Out The Boat And Started Drowning?")
-		sys.exit()
+            elif mailPick == 13:
+                    Discord()
+
+            elif mailPick == 14:
+                    Spotify()
+
+            elif mailPick == 15:
+                    Blockchain()
+
+            elif mailPick == 16:
+                    RiotGames()
+
+            elif mailPick == 17:
+                    Rockstar()
+
+            elif mailPick == 18:
+                    AskFM()
+
+            elif mailPick == 19:
+                    Webhost000()
+
+            elif mailPick == 20:
+                    Dreamteam()
+
+            elif mailPick == 21:
+                    print("Gamehag Coming Soon")
+
+            elif mailPick == 22:
+                    Mega()
+
+            elif mailPick == 30:
+                    MailerMenu()
+
+            elif mailPick == 69:
+                    RedirectionMain()
+
+            elif mailPick == 80:
+                    Languages()
+
+            elif mailPick == 99:
+                    os.system("clear")
+                    print("Hope I See You Soon")
+                    print("Happy Phishing")
+                    sys.exit()
+
+            elif mailPick == 1337:
+                    print("\n" + green + "[" + white + "+" + green + "]" + white + " This Tool Was Created So People Would Have It Easier To Launch Phishing Attacks")
+                    print("\n" + green + "[" + white + "+" + green + "]" + white + " I Do Not Take Any Responsibility For Your Actions")
+                    print("\n" + green + "[" + white + "+" + green + "]" + white + " But I Don't Give A F*ck About What You Do")
+                    print("\n" + green + "[" + white + "+" + green + "]" + white + " More Emails Will Come Soon!")
+                    print("\n" + green + "[" + white + "+" + green + "]" + white + " Contact:")
+                    print(green + "[" + white + "+" + green + "]" + white + " Instagram: BiZk3n (most Active)")
+                    print(green + "[" + white + "+" + green + "]" + white + " Github: BiZken [https://github.com/BiZken]")
+                    CurrentDir()
+                    print(green + "[" + white + "+" + green + "]" + white + " Restart PhishMailer? Y/N")
+
+                    ReRun = input("root@phishmailer:~ " + white)
+                    if ReRun == "Y" or ReRun == "y":
+                            os.system("clear")
+                            mainMenu()
+                    else:
+                            os.system("clear")
+                            print(alert + " shuting Down")
+
+            elif mailPick == 4444:
+                    TODO()
+                    print(start + " Restart PhishMailer? Y/N")
+                    restart = input("root@phishmailer:~ " + white)
+                    if restart == "Y" or restart == "y":
+                            os.system("clear")
+                            mainMenu()
+                    else:
+                            print(alert + " shuting Down")
+                            sys.exit()
+
+            else:
+                    print("\nSomething Went Wrong There Partner")
+                    print("Are You Ok? Did You Fell Out The Boat And Started Drowning?")
+                    sys.exit()
+                    
+        except ValueError:
+            print("\nSomething Went Wrong There Partner")
+            print("Are You Ok? Did You Fell Out The Boat And Started Drowning?")
+            sys.exit()
 
 mainMenu()
